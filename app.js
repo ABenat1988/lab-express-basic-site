5c775734745cf462705086e01042b4b5c2f56e0b
Christoph Weber <WeberChristoph85@gmail.com>
2023-03-14 19:03:08 +0100

    finished photo page

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.use(express.static('public'));
 app.get("/", (request, response, next) => {
     response.sendFile(__dirname + '/views/home.html');
 });
+
+// Route for workspage
 app.get("/works", (request, response, next) => {
     response.sendFile(__dirname + '/views/works.html');
 });
@@ -15,5 +17,12 @@ app.get("/works", (request, response, next) => {
 app.get("/about", (request, response, next) => {
     response.sendFile(__dirname + '/views/about.html');
 });
+
+
+// Route for photopage
+app.get("/photos", (request, response, next) => {
+    response.sendFile(__dirname + '/views/photos.html');
+});
+
 
 app.listen(3000, () => { console.log("server listening on port 3000")});
