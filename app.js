80f029d6bd5a51f8f14b7e2f893407441e0ce2e0
ABenat1988 <ale.benat@gmail.com>
2023-03-14 15:59:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.use(express.static('public'));
+
+app.get("/", (request, response, next) => {
+    response.sendFile(__dirname + '/views/home.html');
+});
+app.listen(3000, () => { console.log("server listening on port 3000")});
