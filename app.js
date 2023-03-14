20ac1d258498b71d91e45e78167195f528cf3634
Christoph Weber <WeberChristoph85@gmail.com>
2023-03-14 17:13:07 +0100

    added  about content

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 app.use(express.static('public'));
 
+
+// Route for homepage
 app.get("/", (request, response, next) => {
     response.sendFile(__dirname + '/views/home.html');
 });
 app.get("/works", (request, response, next) => {
     response.sendFile(__dirname + '/views/works.html');
 });
+
+// Route for aboutpage
+app.get("/about", (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+});
+
 app.listen(3000, () => { console.log("server listening on port 3000")});
